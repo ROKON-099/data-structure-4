fdf3ea163105633255aa9488f0b539415d13ba10
Rokon007 <mdrokonuzzaman099@gmail.com>
2026-03-10 20:33:05 +0600

    2nd commit

---
+++ P13.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cout << "Enter the size of an array:";
+    cin >>n;
+    cout << "Enter " <<n << " Number of array:";
+    int arr[n];
+    for (int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+
+    }
+
+    int largest=arr[0],
+    smallest =arr[0];
+    for (int i=0; i<n; i++)
+    {
+        if (arr[i] >largest)
+            largest=arr[i];
+        if (arr[i]<smallest)
+            smallest=arr[i];
+    }
+    cout << "Largest number is :" <<largest<<endl;
+    cout << "smallest number is :" <<smallest<<endl;
+}
